d2820f3ca60eb8b19621145922c51c1b8ae7a448
Bruno Camargos <brunocamargosn@gmail.com>
2022-08-16 21:02:59 -0300

    added tree node with forEach Depth First

---
+++ src/test/kotlin/tree/TreeNodeTest.kt
@@ -0,0 +1,56 @@
+package tree
+
+import org.junit.jupiter.api.Test
+
+
+class TreeNodeTest {
+
+    @Test
+    fun add() {
+        val hot = TreeNode("Hot")
+        val cold = TreeNode("Cold")
+
+        val beverages = TreeNode("Beverages").run {
+            add(hot)
+            add(cold)
+        }
+    }
+
+    @Test
+    fun `Depth-first traversal`(){
+        val tree = TreeNode("Beverages")
+
+        val hot = TreeNode("Hot")
+        val cold = TreeNode("Cold")
+
+        val tea = TreeNode("Tea")
+        val coffee = TreeNode("Coffee")
+        val chocolate = TreeNode("Chocolate")
+
+        val blackTea = TreeNode("black")
+        val greenTea = TreeNode("green")
+        val chaiTea = TreeNode("chai")
+        val gingerAle = TreeNode("ginger ale")
+        val bitterLemon = TreeNode("bitter lemon")
+        val soda = TreeNode("Soda")
+        val milk = TreeNode("Milk")
+
+        tree.add(hot)
+        tree.add(cold)
+        hot.add(tea)
+        hot.add(coffee)
+        hot.add(chocolate)
+        cold.add(soda)
+        cold.add(milk)
+        tea.add(blackTea)
+        tea.add(greenTea)
+        tea.add(chaiTea)
+        soda.add(gingerAle)
+        soda.add(bitterLemon)
+
+        tree.forEachDepthFirst {
+            println(it.value)
+        }
+
+    }
+}
