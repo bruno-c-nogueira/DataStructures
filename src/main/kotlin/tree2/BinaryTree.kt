04bf268a46ac96063f1535bb6bedd0a577006fc9
bruno-c-nogueira <brunocamargos2009@hotmail.com>
2025-05-01 22:19:49 -0300

    adding binary tree traverse methods

--- src/main/kotlin/tree2/BinaryTree.kt
+++ src/main/kotlin/tree2/BinaryTree.kt
@@ -1,8 +1,12 @@
 package tree2
 
+import queue.QueueArrayList
 import tree.BinaryTree
 
 
+/**
+ * traversal algorithms has both a time and space complexity of O(n)
+ */
 
 typealias Visitor<T> = (T) -> Unit
 
@@ -31,13 +35,14 @@ class BinaryNode<T>(val value: T) {
         } ?: "${root}null\n"
     }
 }
-
+// left -> root -> right
 fun BinaryNode<Int>.inOrder() {
     this.leftChild?.inOrder()
     println(this.value)
     this.rightChild?.inOrder()
 }
 
+// root -> left -> right
 fun preOrder( tree: BinaryNode<Int>?) {
     if (tree == null)
         return
@@ -51,19 +56,52 @@ fun preOrder( tree: BinaryNode<Int>?) {
     // Finally recur on right subtree
     preOrder(tree.rightChild);
 }
+// left -> right -> root
+fun postOrder(tree: BinaryNode<Int>?) {
+    if (tree == null) return
+
+    tree.leftChild?.let { postOrder(it) }
+    tree.rightChild?.let { postOrder(it) }
+    println(tree.value)
+}
+
+fun binaryHeight(tree: BinaryNode<Int>?): Int {
+    if (tree == null) return 0
+     return maxOf(binaryHeight(tree.rightChild) + 1, binaryHeight(tree.leftChild) + 1)
+}
+
+fun treeLevelList(tree: BinaryNode<Int>?): Int {
+    val list = QueueArrayList<BinaryNode<Int>?>()
+
+    list.enqueue(tree)
+    var level = 0
+    while (!list.isEmpty) {
+
+        repeat(list.count) {
+            val dequeue = list.dequeue()
+            dequeue?.leftChild?.let { list.enqueue(dequeue.leftChild) }
+            dequeue?.rightChild?.let { list.enqueue(dequeue.rightChild) }
+        }
+        level +=1
+    }
+    return level
+}
 
 fun main() {
-    val zero = BinaryNode(0)
-    val one = BinaryNode(1)
-    val five = BinaryNode(5)
-    val seven = BinaryNode(7)
-    val eight = BinaryNode(8)
-    val nine = BinaryNode(9)
-    seven.leftChild = one
-    one.leftChild = zero
-    one.rightChild = five
-    seven.rightChild = nine
-    nine.leftChild = eight
-    val tree = seven
-    println(preOrder(tree))
+    val root = BinaryNode(1)
+
+    root.leftChild = BinaryNode(2)
+    root.rightChild = BinaryNode(3)
+
+    root.leftChild?.leftChild = BinaryNode(4)
+    root.leftChild?.rightChild = BinaryNode(5)
+
+    root.rightChild?.leftChild = BinaryNode(6)
+    root.rightChild?.rightChild = BinaryNode(7)
+
+    root.leftChild?.leftChild?.leftChild = BinaryNode(8)
+    root.leftChild?.leftChild?.leftChild?.leftChild = BinaryNode(10)
+
+    root.rightChild?.leftChild?.rightChild = BinaryNode(9)
+    println(treeLevelList(root))
 }
